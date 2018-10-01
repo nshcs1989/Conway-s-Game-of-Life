98776e40071552bc45a72b7125e26260b50a42b5
nshcs1989 <43753930+nshcs1989@users.noreply.github.com>
2018-10-01 18:43:03 +0300

    Create MainGameOfLife.java
    This class represent the main method of Conway's game of life.

---
+++ MainGameOfLife.java
@@ -0,0 +1,32 @@
+
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+public class MainGameOfLife 
+{
+	public static void main(String[] args)
+	{
+		JOptionPane.showMessageDialog(null,"Welcome to Conway’s Game Of Life\nThe following window will show you the first generation");
+		JFrame window=new JFrame();
+		MatrixGameOfLife m = new MatrixGameOfLife(10,10);
+		window.add(m);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(600,600);
+		window.setVisible(true);
+		
+		String str=JOptionPane.showInputDialog("If you want to move for the next generation please type 'YES', else type 'NO'\n"); 
+		
+		while(str.equals("YES"))
+		{
+			m.nextGen();
+			window.add(m);
+			window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			window.setSize(600,600);
+			window.setVisible(true);
+			m.repaint();
+			str=JOptionPane.showInputDialog("If you want to move for the next generation please type 'YES', else type 'NO'\n"); 
+		}
+		
+		
+		
+	}
+}
